5581db0b86d573fc8f270ece3c7b2ac5ee0b39f2
wahidpatel05 <145665859+wahidpatel05@users.noreply.github.com>
2025-05-25 06:27:31 +0000

    datatpe_conversion

---
+++ 03_datatype_conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+// console.log(typeof score)
+
+let valueInNumnber = Number(score)
+
+// console.log(typeof valueInNumnber)
+// console.log(valueInNumnber)
+
+/*
+"33" => 33
+"33abc" => NaN not a number 
+1 => true
+0 => false
+
+*/
+let isLoggedin = 1
+
+let booleanInput = Boolean(isLoggedin)
+console.log(booleanInput);
+
+// numer => boolean conversion can also be done
+/* 
+1 => true
+0 => false
+"" => false
+"wahid" => true 
+*/
