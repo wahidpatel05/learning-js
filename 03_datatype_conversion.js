0ef1cf60dfa1fc8b6bf2f98bab746505ce7327d6
wahidpatel05 <145665859+wahidpatel05@users.noreply.github.com>
2025-05-27 07:21:46 +0000

    number and math

--- 03_datatype_conversion.js
+++ 03_datatype_conversion.js
@@ -39,4 +39,25 @@ console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
 
 let name = "wahid"
-console.log("My name is: "+name);
+// console.log("My name is: "+name);
+
+const myName = new String('wahid');
+console.log(myName);
+
+// methods of the strings
+console.log(`the string ends with d? ${myName.endsWith("d")}`)
+console.log(`the string length is ${myName.length}`);
+console.log(`${myName.concat(" patel")}`)
+console.log(`${myName.charCodeAt(0)}`); //it returns unicode value it is between 0 and 
+
+// let user = new String('{')
+// console.log(`${user.charCodeAt(0)}`);
+
+console.log(`${myName.toUpperCase()}`);
+
+// substring doesnt take negative values but SLICE method takes negative values just like python
+
+
+
+
+
---
+++ 04_number_maths.js
@@ -0,0 +1,18 @@
+const number = new Number(40000)
+
+// console.log(number.toLocaleString('en-IN'));
+
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2));
+
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random()*(max - min ) + min))
+
+
+
+
+
